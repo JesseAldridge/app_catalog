2c1849e37d3cd5f18bc9ef87619a33b473071d95
Jesse Aldridge <jessealdridge@gmail.com>
2018-09-25 05:05:44 -0700

    catalog

---
+++ apps/hapi_list/server.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const Hapi = require('hapi');
+
+const server = Hapi.server({
+  port: 80,
+  host: '0.0.0.0',
+});
+
+const init = async () => {
+  await server.register(require('inert'));
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+      return h.file('./public/hello.html');
+    }
+  });
+
+  await server.start();
+  console.log(`Server running at: ${server.info.uri}`);
+};
+
+process.on('unhandledRejection', (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
