14f38cf52e0184a6fb5737bef63d7752f7cc76c7
Jesse Aldridge <jessealdridge@gmail.com>
2018-09-27 20:36:40 -0700

    vote-site-firebase

--- apps/vote-site-firebase/README.txt
+++ apps/vote-site-firebase/README.txt
@@ -5,5 +5,3 @@ Admin SDK Docs: https://firebase.google.com/docs/admin/setup
 Download json and put them in secrets dir:
 Firebase console -> Select App -> Gear Icon -> Project Settings -> Service Accounts
 https://console.firebase.google.com/u/0/project/hello-firebase-server/settings/serviceaccounts/adminsdk
-
-npm install firebase
--- apps/vote-site-firebase/server.js
+++ apps/vote-site-firebase/server.js
@@ -1,13 +1,65 @@
-var admin = require("firebase-admin");
+'use strict';
+
+const Hapi = require('hapi');
+const Mustache = require('mustache');
+const Admin = require("firebase-admin");
 
 var serviceAccount = require("./secrets/vote-site-fc0ad-firebase-adminsdk-8ij3z-b93b0f328c.json");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+Admin.initializeApp({
+  credential: Admin.credential.cert(serviceAccount),
   databaseURL: "https://vote-site-fc0ad.firebaseio.com"
 });
 
-admin.database().ref('/').set({
-    username: "test",
-    email: "test@mail.com"
+const server = Hapi.server({
+  port: 80,
+  host: '0.0.0.0',
 });
+
+const init = async () => {
+  await server.register(require('inert'));
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+      const promise = new Promise((resolve, reject) => {
+        Admin.database().ref('documents').once('value', snapshot => {
+          const template_str = `
+          <link rel='stylesheet' href='https://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css'>
+
+          <style>
+            .wrapper { margin: 5px }
+            .post { margin: 5px }
+          </style>
+
+          <div class='wrapper'>
+            {{#doc}}
+              <div class='post'>
+                <button>🌯</button>
+                <button>🌮</button>
+                <b>{{title}}</b><span style="margin: 5px">{{upvotes}}/{{downvotes}}<span>
+              </div>
+            {{/doc}}
+          </div>
+          `;
+
+          const html = Mustache.render(template_str, {doc: snapshot.val()});
+          resolve(html);
+        });
+      });
+
+      return promise;
+    }
+  });
+
+  await server.start();
+  console.log(`Server running at: ${server.info.uri}`);
+};
+
+process.on('unhandledRejection', (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
